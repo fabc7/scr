3b6d7828a166ab98ae008cd7287d9f0d14c04348
fabc7 <fabiancorreapavez@gmail.com>
2026-05-01 20:02:59 -0400

    Update recorder.py

--- recorder/recorder.py
+++ recorder/recorder.py
@@ -101,7 +101,8 @@ async def record_stream(profile_url):
                 
                 seconds_without_data = 0
                 previous_size = 0
-                MAX_BYTES = 5 * 1024 * 1024 * 1024
+                # MAX_BYTES = 5 * 1024 * 1024 * 1024
+                # MAX_BYTES = 10 * 1024 * 1024
                 
                 while True:
                     await asyncio.sleep(5)
